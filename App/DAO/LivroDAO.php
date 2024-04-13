6c68d4eba157e620ed174aefc745a249f7a34cf2
Núbia Torres <115761355+nubyoli@users.noreply.github.com>
2024-04-13 13:46:07 -0300

    Update LivroDAO.php

--- App/DAO/LivroDAO.php
+++ App/DAO/LivroDAO.php
@@ -23,13 +23,50 @@ class LivroDAO
         $stmt->execute();
     }
 
-    public function update()
+    public function update(LivroModel $model)
     {
+        $sql = "UPDATE Livros SET titulo=?, autor=?, data_publicacao=? WHERE id=?";
 
+        $stmt = $this->conexao->prepare($sql);
+
+        $stmt->bindValue(1, $model->titulo);
+        $stmt->bindValue(2, $model->autor);
+        $stmt->bindValue(3, $model->data_publicacao);
+        $stmt->bindValue(4, $model->id);
+
+        $stmt->execute();
     }
 
     public function select()
     {
+        $sql = "SELECT * FROM Livros";
 
+        $stmt = $this->conexao->prepare($sql);
+        $stmt->execute();
+
+        return $stmt->fetchAll(PDO::FETCH_CLASS);
     }
+
+    public function selectById(int $id)
+    {
+        include_once 'Model/LivroModel.php';
+
+        $sql = "SELECT * FROM Livros WHERE id=?";
+
+        $stmt = $this->conexao->prepare($sql);
+        $stmt->bindValue(1, $id);
+        $stmt->execute();
+
+        return $stmt->fetchObject("LivroModel");
+    }
+
+    public function delete(int $id)
+    {
+        $sql = "DELETE * FROM Livros WHERE id= ?";
+
+        $stmt = $this->conexao->prepare($sql);
+        $stmt->bindValue(1, $id);
+        $stmt->execute();
+    }
+
 }
