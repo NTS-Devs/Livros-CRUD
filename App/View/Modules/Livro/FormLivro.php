a76a78b2ff91e5414b9557cac062a73b2efa2384
João Vitor Lima <108885897+SenhorLime@users.noreply.github.com>
2024-04-13 13:37:27 -0300

    Adicionando CSS ao FormLivro

--- App/View/Modules/Livro/FormLivro.php
+++ App/View/Modules/Livro/FormLivro.php
@@ -1,17 +1,35 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.min.css" />
   <title>Cadastro de Livros</title>
+
+  <style>
+    body {
+      width: 100vw;
+      height: 100vh;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+    }
+
+    fieldset {
+      width: 85%;
+    }
+  </style>
 </head>
+
 <body>
   <fieldset>
-    <legend>Cadastro de Livros</legend>
+    <h2>Cadastro de Livros</h2>
 
     <form method="post" action="<?php echo BASE_URL ?>/livros/form/save">
       <label for="titulo">Titulo: </label>
       <input value="<?= $model->titulo ?>" type="text" name="titulo" id="titulo" required>
+
 
       <label for="autor">Autor: </label>
       <input value="<?= $model->autor ?>" type="text" name="autor" id="autor" required>
@@ -23,4 +41,5 @@
     </form>
   </fieldset>
 </body>
+
 </html>
