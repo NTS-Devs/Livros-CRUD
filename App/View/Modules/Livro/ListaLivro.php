6f51cd5bbb18b7bbff75481c453b5092c44a5c7b
João Vitor Lima <joao.zeferino@aluno.cefetmg.br>
2024-04-15 16:59:24 -0300

    Removendo o CSS dentro arquivo

--- App/View/Modules/Livro/ListaLivro.php
+++ App/View/Modules/Livro/ListaLivro.php
@@ -5,87 +5,80 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.min.css" />
+  <link rel="stylesheet"
+    href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+
+  <link rel="stylesheet" href="<?= BASE_URL . "/View/Modules/Livro/CSS/styles.css" ?>">
+  <link rel="stylesheet" href="<?= BASE_URL . "/View/Modules/Livro/CSS/styles-mobile.css" ?>">
+  <link rel="shortcut icon" href="<?= BASE_URL . "/View/Modules/Livro/Assets/favicon.ico" ?>" type="image/x-icon">
   <title>Cadastro de Livros</title>
-
-  <style>
-    body {
-      width: 100vw;
-      display: flex;
-      align-items: center;
-      justify-content: center;
-    }
-
-    table {
-      width: 90%;
-    }
-
-    th,
-    td {
-      text-align: center;
-      max-width: 200px;
-    }
-
-    .grid {
-      margin: 10px;
-    }
-
-    button {
-      border: 0;
-      padding: 10px;
-    }
-
-    .warning {
-      background-color: #E12E1E;
-    }
-  </style>
 </head>
 
-<body>
-  <table>
-    <thead>
-      <tr>
-        <th>ID</th>
-        <th>Título</th>
-        <th>Autor</th>
-        <th>Data de Publicação</th>
-        <th>Ações</th>
-      </tr>
-    </thead>
-    <tbody>
-      <?php foreach ($model->rows as $livro) { ?>
+<body class="grid-container">
+
+  <div class="sidebar grid-item">
+    <a href="<?= BASE_URL ?>">
+      <span class="material-symbols-outlined">
+        menu_book
+      </span>
+      <span>Livros</span>
+    </a>
+
+    <a href="<?= BASE_URL . "/livros/form" ?>">
+      <span class="material-symbols-outlined">
+        add_circle
+      </span>
+      <span>Novo Livro</span>
+    </a>
+  </div>
+
+  <div class="main-container grid-item">
+    <table>
+      <thead>
         <tr>
-          <td><?= $livro->id ?></td>
-          <td><?= $livro->titulo ?></td>
-
-          <td><?= $livro->autor ?></td>
-          <td><?= $livro->data_publicacao ?></td>
-          <td>
-            <div class="grid">
-              <div>
-                <a href="<?= BASE_URL ?>/livros/form?id=<?= $livro->id ?>">
-                  <button>Editar</button>
-                </a>
-              </div>
-
-              <div>
-                <a href="<?= BASE_URL ?>/livros/delete?id=<?= $livro->id ?>">
-                  <button class="warning">Deletar</button>
-                </a>
-              </div>
-
-            </div>
-          </td>
+          <th>ID</th>
+          <th>Título</th>
+          <th>Autor</th>
+          <th>Data de Publicação</th>
+          <th>Ações</th>
         </tr>
-      <?php } ?>
+      </thead>
+      <tbody>
+        <?php foreach ($model->rows as $livro) { ?>
+          <tr>
+            <td><?= $livro->id ?></td>
+            <td><?= $livro->titulo ?></td>
 
-      <?php if (count($model->rows) == 0) { ?>
-        <tr>
-          <td colspan="5">Nenhum registro encontrado</td>
-        </tr>
-      <?php } ?>
+            <td><?= $livro->autor ?></td>
+            <td><?= $livro->data_publicacao ?></td>
+            <td>
+              <div class="grid">
+                <div>
+                  <a href="<?= BASE_URL ?>/livros/form?id=<?= $livro->id ?>">
+                    <button>Editar</button>
+                  </a>
+                </div>
 
-    </tbody>
-  </table>
+                <div>
+                  <a href="<?= BASE_URL ?>/livros/delete?id=<?= $livro->id ?>">
+                    <button class="warning">Deletar</button>
+                  </a>
+                </div>
+
+              </div>
+            </td>
+          </tr>
+        <?php } ?>
+
+        <?php if (count($model->rows) == 0) { ?>
+          <tr>
+            <td colspan="5">Nenhum registro encontrado</td>
+          </tr>
+        <?php } ?>
+
+      </tbody>
+    </table>
+  </div>
 </body>
 
 </html>
